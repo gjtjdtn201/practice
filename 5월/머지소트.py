a5b768a361cca40548198e8129a743b07a8a77eb
허성수 <gjtjdtn201@naver.com>
2020-05-14 17:23:23 +0900

    0514 집

---
+++ 5월/머지소트.py
@@ -0,0 +1,41 @@
+def merge(list1, list2):
+    global cnt
+    i = 0
+    j = 0
+    merged_list = []
+    if list1[-1] > list2[-1]:
+        cnt += 1
+    while i < len(list1) and j < len(list2):
+        if list1[i] > list2[j]:
+            merged_list.append(list2[j])
+            j += 1
+        else:
+            merged_list.append(list1[i])
+            i += 1
+
+    while i < len(list1):
+        merged_list.append(list1[i])
+        i += 1
+    while j < len(list2):
+        merged_list.append(list2[j])
+        j += 1
+
+    return merged_list
+
+def merge_sort(my_list):
+    if len(my_list) <= 1:
+        return my_list
+    mid = len(my_list)//2
+    left = merge_sort(my_list[:mid])
+    right = merge_sort(my_list[mid:])
+    return merge(left, right)
+
+import sys
+sys.stdin = open('(5204)병합정렬_input.txt')
+
+for tc in range(1, int(input())+1):
+    N = int(input())
+    a = list(map(int, input().split()))
+    cnt = 0
+    b = merge_sort(a)
+    print('#{} {} {}'.format(tc, b[N//2], cnt))
--- 백준/Gold/파이프 옮기기1.py
+++ 백준/Gold/파이프 옮기기1.py
@@ -1,41 +1,19 @@
 import sys
 sys.stdin = open('파이프 옮기기1.txt', 'r')
 
-def SP(x, y, shape):
-    global cnt
-    if x == N-1 and y == N-1:
-        cnt += 1
-        return
-
-    if shape == 1:
-        if y+1 < N and matrix[x][y+1] == 0:
-            SP(x, y+1, 1)
-
-        if x+1 < N and y+1 < N and matrix[x][y+1] == 0 and matrix[x+1][y] == 0 and matrix[x+1][y+1] == 0:
-            SP(x+1, y+1, 3)
-    elif shape == 2:
-        if x + 1 < N and matrix[x+1][y] == 0:
-            SP(x+1, y, 2)
-
-        if x + 1 < N and y + 1 < N and matrix[x][y + 1] == 0 and matrix[x + 1][y] == 0 and matrix[x + 1][y + 1] == 0:
-            SP(x + 1, y + 1, 3)
-    else:
-        if y + 1 < N and matrix[x][y + 1] == 0:
-            SP(x, y + 1, 1)
-
-        if x + 1 < N and matrix[x+1][y] == 0:
-            SP(x+1, y, 2)
-
-        if x + 1 < N and y + 1 < N and matrix[x][y + 1] == 0 and matrix[x + 1][y] == 0 and matrix[x + 1][y + 1] == 0:
-            SP(x + 1, y + 1, 3)
-
 N = int(input())
+matrix = [list(map(int, input().split())) for _ in range(N)]
+visit = [[[0]*3 for _ in range(N)] for __ in range(N)]
+visit[0][1][0] = 1
+for x in range(2, N):
+    if matrix[0][x] == 0:
+        visit[0][x][0] = visit[0][x-1][0]
+for y in range(1, N):
+    for x in range(2, N):
+        if matrix[y][x]+matrix[y][x-1]+matrix[y-1][x] == 0:
+            visit[y][x][2] = visit[y-1][x-1][0] + visit[y-1][x-1][1] + visit[y-1][x-1][2]
+        if matrix[y][x] == 0:
+            visit[y][x][0] = visit[y][x-1][0] + visit[y][x-1][2]
+            visit[y][x][1] = visit[y-1][x][1] + visit[y-1][x][2]
 
-matrix = []
-
-for i in range(N):
-    matrix.append(list(map(int, input().split())))
-cnt = 0
-SP(0, 1, 0)
-
-print(cnt)
+print(sum(visit[N-1][N-1]))
