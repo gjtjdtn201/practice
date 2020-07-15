054ec699945c82cbc736ba8279063e0632e10dd8
허성수 <gjtjdtn201@naver.com>
2020-07-16 00:20:33 +0900

    0716 집

--- 백준/Gold/미로 탈출하기.py
+++ 백준/Gold/미로 탈출하기.py
@@ -4,19 +4,28 @@ sys.stdin = open('미로 탈출하기.txt')
 import sys
 input = sys.stdin.readline
 
-def DFS(sty, stx):
+def DFS(sty, stx, color):
     global cnt
-    stack = [sty, stx]
-    bt = [(sty, stx)]
+    stack = [(sty, stx)]
+    visit[sty][stx] = color
+    moves = 0
     while stack:
         a, b = stack.pop()
-        if 0 > sty or N <= sty or 0 > stx or M <= stx:
-            visit[sty][stx]
-            for by, bx in bt:
-                visit[by][bx] = 1
-                cnt += 1
-            return
-
+        moves += 1
+        dy, dx = move[matrix[a][b]]
+        ny = a + dy
+        nx = b + dx
+        if 0 > ny or N <= ny or 0 > nx or M <= nx:
+            cnt += moves
+            return True
+        if visit[ny][nx] == 0:
+            visit[ny][nx] = color
+            stack.append((ny, nx))
+            continue
+        if colors[visit[ny][nx]] == 1:
+            cnt += moves
+            return True
+        return False
 
 
 N, M = map(int, input().split())
@@ -28,11 +37,15 @@ move = {'U': (-1, 0), 'R': (0, 1), 'D': (1, 0), 'L': (0, -1)}
 visit = [[0]*M for _ in range(N)]
 
 cnt = 0
+chk = 1
+colors = {}
 
 for y in range(N):
     for x in range(M):
-        if not visit[y][x]:
-            if DFS(y, x):
-                cnt += 1
-        else:
-            cnt += 1
+        if visit[y][x] == 0:
+            colors[chk] = 0
+            if DFS(y, x, chk):
+                colors[chk] = 1
+            chk += 1
+
+print(cnt)
