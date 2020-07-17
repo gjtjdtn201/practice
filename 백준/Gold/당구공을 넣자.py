2efd381d547044cb3e2bfd853840a297f9ff1c30
허성수 <gjtjdtn201@naver.com>
2020-07-18 01:11:11 +0900

    0718 집

---
+++ 백준/Gold/당구공을 넣자.py
@@ -0,0 +1,12 @@
+import sys
+sys.stdin = open('당구공을 넣자.txt')
+
+M, N, x, y, mx, my = map(int, input().split())
+
+stack = [(y, x)]
+while stack:
+    a, b = stack.pop()
+    ny, nx = a, b
+    while 0 <= ny < N and 0 <= nx < M:
+        ny += my
+        nx += mx
