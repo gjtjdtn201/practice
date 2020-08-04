1f2fa8a4763a6f1694ea830d70d3373427d6037b
허성수 <gjtjdtn201@naver.com>
2020-08-04 23:16:52 +0900

    0804 집

---
+++ 백준/Silver/온라인 판매.py
@@ -0,0 +1,18 @@
+import sys
+sys.stdin = open('온라인 판매.txt')
+
+N, M = map(int, input().split())
+
+cus = []
+for i in range(M):
+    cus.append(int(input()))
+cus.sort(reverse=True)
+cnt, cnt2 = 0, 0
+for i in range(1, M+1):
+    if i > N:
+        break
+    if cus[i-1]*i > cnt:
+        cnt = cus[i-1]*i
+        cnt2 = cus[i-1]
+
+print(cnt2, cnt)
