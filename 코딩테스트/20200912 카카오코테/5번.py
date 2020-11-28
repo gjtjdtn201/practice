ea45582b2773616b2b8f350b927559210009d89f
허성수 <gjtjdtn201@naver.com>
2020-11-28 09:55:37 +0900

    FIX directory

--- 코딩테스트/20200912 카카오코테/5번.py
+++
@@ -1,7 +0,0 @@
-def solution(board, r, c):
-    answer = 0
-
-    return answer
-
-board, r, c = [[1,0,0,3],[2,0,0,0],[0,0,0,2],[3,0,1,0]], 1, 0
-print(solution(board, r, c))
--- 코딩테스트/20201114 11번가/2번.py
+++
@@ -1,9 +0,0 @@
-A = [5, 10, 18, 7, 8, 3]
-N = len(A)
-A.sort(reverse=True)
-ans = -1
-for i in range(N-2):
-    if A[i] < A[i+1]+A[i+2]:
-        ans = A[i] + A[i+1] + A[i+2]
-        break
-print(ans)
